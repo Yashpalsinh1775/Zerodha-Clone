7c50e0a0ff290970839c91841c6ef05b28e332ea
Yashpalsinh <yashpalsinhjadeja1775@gmail.com>
2025-07-27 11:13:31 +0530

    Update Hero.js

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -5,9 +5,18 @@ function Hero() {
         <div className='container p-5 mb-5'>
             <div className='row text-center'>
                 <img src='media/images/homeHero.png' alt='Hero Images' className='mb-5'/>
-                <h1 className='mt-5'>Inverst in everything</h1>
-                <p>Online plateform to inverst in stocks, derivatives, mutual funds, and more.</p>
-                <button className='p-2 btn btn-primary fs-5' style={{width:"20%", margin:"0 auto",}}>SignUp Now</button>
+                <h1 className='mt-5'>Invest in everything</h1>
+                <p>Online platform to invest in stocks, derivatives, mutual funds, and more.</p>
+
+                <a 
+                    href='https://zerodha-clone-dashboard-psk3.onrender.com'
+                    target='_blank'  
+                    rel='noopener noreferrer' 
+                    className='p-2 btn btn-primary fs-5'
+                    style={{ width: "20%", margin: "0 auto", display: "inline-block" }}
+                >
+                    SignUp Now
+                </a>
             </div>
         </div>
     );

f6eb63d95aafa3a3ba84821b5f46b20608ad6ab4
Yashpalsinh <yashpalsinhjadeja1775@gmail.com>
2025-08-04 22:27:04 +0530

    Update Awards.js

--- frontend/src/landing_page/home/Awards.js
+++ frontend/src/landing_page/home/Awards.js
@@ -1,32 +1,37 @@
-<div className='container mt-5'>
-  <div className='row'>
-    <div className='col-12 col-md-6 p-4'>
-      <img src='media/images/largestBroker.svg' alt='' className='img-fluid' />
-    </div>
-    <div className='col-12 col-md-6 p-4'>
-      <h1 className='fs-3 fs-md-1'>Largest stock broker in India</h1>
-      <p className='mb-4'>
-        2+ million Zerodha clients contribute to over 15% of all volumes in India daily by trading and investing in:
-      </p>
+import React from 'react';
 
-      <div className='row'>
-        <div className='col-6'>
-          <ul>
-            <li><p>Futures and Options</p></li>
-            <li><p>Commodity derivatives</p></li>
-            <li><p>Currency derivatives</p></li>
-          </ul>
+function Education() {
+    return (
+          <div className='container mt-5'>
+             <div className='row'>
+             <div className='col-12 col-md-6 p-4'>
+          <img src='media/images/largestBroker.svg' alt='' className='img-fluid' />
         </div>
-        <div className='col-6'>
-          <ul>
-            <li><p>Stocks & IPOs</p></li>
-            <li><p>Direct mutual funds</p></li>
-            <li><p>Bonds and Grow</p></li>
-          </ul>
+        <div className='col-12 col-md-6 p-4'>
+          <h1 className='fs-3 fs-md-1'>Largest stock broker in India</h1>
+          <p className='mb-4'>
+            2+ million Zerodha clients contribute to over 15% of all volumes in India daily by trading and investing in:
+          </p>
+    
+          <div className='row'>
+            <div className='col-6'>
+              <ul>
+                <li><p>Futures and Options</p></li>
+                <li><p>Commodity derivatives</p></li>
+                <li><p>Currency derivatives</p></li>
+              </ul>
+            </div>
+            <div className='col-6'>
+              <ul>
+                <li><p>Stocks & IPOs</p></li>
+                <li><p>Direct mutual funds</p></li>
+                <li><p>Bonds and Grow</p></li>
+              </ul>
+            </div>
+          </div>
+    
+          <img src='media/images/pressLogos.png' className='img-fluid mt-4' alt='' />
         </div>
       </div>
-
-      <img src='media/images/pressLogos.png' className='img-fluid mt-4' alt='' />
     </div>
-  </div>
-</div>
+)

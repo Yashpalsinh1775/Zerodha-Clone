06d2e238b3a13215e947695cd9005ea22bb72c43
Yashpalsinh <yashpalsinhjadeja1775@gmail.com>
2025-08-04 22:19:44 +0530

    Update HomePage.js

--- frontend/src/landing_page/home/HomePage.js
+++ frontend/src/landing_page/home/HomePage.js
@@ -4,8 +4,8 @@ import Awards from './Awards';
 import Stats from './Stats';
 import Pricing from './Pricing';
 import Education from './Education';
-import OpenAccount from '../OpenAccount';
 
+import OpenAccount from '../OpenAccount';
 
 function HomePage() {
     return (
